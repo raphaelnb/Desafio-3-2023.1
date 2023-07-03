00ebb1d041e9db8f7461e52c869cb57c62ef36bf
Raphael Nobre <nobre.roots@gmail.com>
2023-07-02 21:58:50 -0300

    add a api dos municípios, e os exibe numa tabela

--- index.js
+++ index.js
@@ -22,3 +22,38 @@ getUF().then((ufs) => {
         listaEfs.appendChild(option)
     }
 })
+
+function handleSubmit() {
+    
+    
+    
+        async function getMunicipio(UF) {
+            const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UF}/municipios`)
+            const data = await response.json()
+            
+            return data.map((i) => i.nome)
+        }
+
+    const ufVal = document.getElementById('uf')
+    const UF = ufVal.value
+
+    getMunicipio(UF).then((muns) => {
+    const tabela = document.getElementById('tabela')
+    tabela.innerHTML = ''
+        
+        for (let i = 0; i < muns.length; i++) {
+            const nomeMun = muns[i]
+
+            const celula = document.createElement('div')
+            celula.textContent = nomeMun
+            
+            tabela.appendChild(celula)
+
+            celula.classList.add('cel-tabela')
+            
+            if ((i + 1) % 3 === 0) {
+                celula.classList.add('nova-linha')
+            }
+        }
+    })
+}
